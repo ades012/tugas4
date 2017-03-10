d822489fde67dc3192b3be1c365c8f35906f0533
ades012 <gngsptn@gmail.com>
2017-03-10 11:39:32 +0700

    pembuatan var kepegawaian

---
+++ kepegawaian.java
@@ -0,0 +1,15 @@
+public class kepegawaian {
+	public static void main (String[] args) {
+	manager bos = new manager ("Supardi", "Bojong Koneng', 6750000);
+	bos.setBonus(1500000);
+
+		employee[] pegawai = new employee [3];
+		pegawai [0] = bos;
+		pegawai [1] = new staff("Ades Deliano", "Banjaran", 3000000);
+		pegawai [2] = new staff("Giofanni Mowoka", "Suka Miskin", 2400000);
+
+		for (int i = 0; i<pegawai.length; i++) {
+		System.out.println(pegawai[i].getName()+"\t"+pegawai[i].getAddress()+"\t"+pegawai[i].getSalary());
+		}
+	}
+}
